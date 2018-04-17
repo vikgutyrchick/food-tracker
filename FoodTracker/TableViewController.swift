bbbe091f89ce57d433cea3779aa48a05f0e5d5e8
Viktoria Gutyrchick <vikgutyrchick@gmail.com>
2018-04-17 19:46:03 +0300

    Load initial data

--- FoodTracker/TableViewController.swift
+++ FoodTracker/TableViewController.swift
@@ -16,12 +16,9 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // Load the sample data
+        loadSampleMeals()
     }
 
     override func didReceiveMemoryWarning() {
