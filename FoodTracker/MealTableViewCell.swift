e2e4771c010f011ad029174c9d67d0d2b4181353
Viktoria Gutyrchick <vikgutyrchick@gmail.com>
2018-04-17 19:06:22 +0300

    Design custom table cells(add label, image and horizontal stack view).

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Viktoria Gutyrchick on 17.04.2018.
+//  Copyright © 2018 Viktoria. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
