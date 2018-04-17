054baaa5114b34e5d15253f8810ef4472f5dd7c9
Viktoria Gutyrchick <vikgutyrchick@gmail.com>
2018-04-17 11:55:19 +0300

    Add new Swift file "Rating Control" and create a custom view.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,9 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Viktoria Gutyrchick on 17.04.2018.
+//  Copyright © 2018 Viktoria. All rights reserved.
+//
+
+import Foundation
---
+++ RatingControl.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class RatingControl: UIStackView {
+
+// MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
