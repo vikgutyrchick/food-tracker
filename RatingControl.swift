2550cb7166de6fac4efbd39fbe3ac193275da9da
Viktoria Gutyrchick <vikgutyrchick@gmail.com>
2018-04-17 12:56:42 +0300

    In Rating Control write code which add buttons to the View

--- RatingControl.swift
+++ RatingControl.swift
@@ -1,13 +1,67 @@
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+    
+    // MARK: Properties
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
 
-// MARK: Initialization
+    // MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
     }
+    
+    // MARK: Buttin Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Нажата кнопка 😀")
+    }
+    
+    // MARK: Private Methods
+    private func setupButtons() {
+        
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
+        
+        // Create the Button
+        let button = UIButton()
+        button.backgroundColor = UIColor.purple
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints  = false
+        button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+        button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+        }
+        
+    }
+    
 }
